a05e5cf4b45aaf0a4d7e94473d8ecba9a197fc85
Leonardo Duarte <18leonardoduarte@gmail.com>
2023-06-07 22:08:44 -0300

    feat[sign-in]: sign in with email and password

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -5,13 +5,30 @@ import Input from "../Input";
 import "./styles.css";
 import { signInWithGoogle } from "../../../util/loginWithGoogle";
 import { useRouter } from 'next/navigation';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginForm() {
   const router = useRouter();
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // login with email and password in firebase
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+
+        // coisa de pleno:
+        if (user.email == "admin@centrodecoleta.com.br") {
+          router.push('/dashboard');
+        } else {
+          router.push('/agendamento');
+        }
+      })
   }
 
   return (
@@ -20,8 +37,8 @@ export default function LoginForm() {
         <h1 className="title">Login</h1>
         <form className="form" onSubmit={handleSubmit}>
           <Input label="E-mail:" type="email" onChange={(e) => setEmail(e.target.value)} />
-          <Input label="Senha:" type="password" />
-          <Button label="Login" />
+          <Input label="Senha:" type="password" onChange={(e) => setPassword(e.target.value)} />
+          <Button label="Login" onClick={handleSubmit} />
           <br />
           {/* SIM EU USEI UM FUCKING BR PRA ABRIR ESPAÇO ASDLKJFASKFJSKLFJLSDÇ */}
           <button className="btn" onClick={(e) => { e.preventDefault(); signInWithGoogle(router) }}>

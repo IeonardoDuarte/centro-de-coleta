c31ebc647ea3e353498b9781ec9d0d71513da4ce
Leonardo Duarte <18leonardoduarte@gmail.com>
2023-06-07 22:38:09 -0300

    fix[scheduling]: date picker not changing value

--- src/components/DatePickerInput/index.js
+++ src/components/DatePickerInput/index.js
@@ -4,7 +4,7 @@ export default function DatePickerInput({ label, onChange}) {
   return (
     <div className="date_picker">
       <label htmlFor="pickupDate">{label}</label>
-      <input type="date" id="pickupDate" name="pickupDate" onChange={onChange} defaultValue={new Date()} value={'2023-06-07'}></input>
+      <input type="date" id="pickupDate" name="pickupDate" onChange={onChange} defaultValue={'2023-06-07'}></input>
     </div>
   )
 }
